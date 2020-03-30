c448b9b8b8fa26dedb2f5e39756c75de3f03c7bb
arnar44 <arnar44@gmail.com>
2020-03-30 21:16:32 +0200

    return an array instead of json and allow headers

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const football = require('./routes/football');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/highlights', highlights);
