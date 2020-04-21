904fb1266f3391d1733f35aa773a1a8eb80310fa
arnar44 <arnar44@gmail.com>
2020-04-21 17:31:23 +0200

    Static data processed and returned

--- routes/fantasy.js
+++ routes/fantasy.js
@@ -70,9 +70,80 @@ function shouldFetch(d2) {
   return false;
 }
 
-async function fetchStatic() {
-  const bootstrapUrl = 'https://fantasy.premierleague.com/api/bootstrap-static/';
-  const response = await fetch(bootstrapUrl);
+function getGameWeek(events) {
+  for (let i = 0; i < events.length; i += 1) {
+    if (events[i].is_current) {
+      return events[i];
+    }
+  }
+  // If season over and none is true, return final week
+  return events[events.length - 1];
+}
+
+function getPlayer(id, players) {
+  for (let i = 0; i < players.length; i += 1) {
+    if (players[i].id === id) {
+      return {
+        name: players[i].second_name,
+        position: players[i].element_type,
+      };
+    }
+  }
+
+  return { name: 'not found', position: 'Not found' };
+}
+
+function processStatic(data, players) {
+  return {
+    data: {
+      id: data.gameweek.id,
+      name: data.gameweek.name,
+      average_score: data.gameweek.average_entry_score,
+      highest_score: data.gameweek.highest_score,
+      chip_plays: [
+        { name: 'Bench Boost', played: data.gameweek.chip_plays[0].num_played },
+        { name: 'Free Hit', played: data.gameweek.chip_plays[1].num_played },
+        { name: 'Wild Card', played: data.gameweek.chip_plays[2].num_played },
+        { name: 'Triple Captain', played: data.gameweek.chip_plays[3].num_played },
+      ],
+      most_selected: getPlayer(data.gameweek.most_selected, players).name,
+      most_transferred_in: getPlayer(data.gameweek.most_transferred_in, players).name,
+      top_player: {
+        name: getPlayer(data.gameweek.top_element, players).name,
+        points: data.gameweek.top_element_info.points,
+      },
+      transfers_made: data.gameweek.transfers_made,
+      most_captained: getPlayer(data.gameweek.most_captained, players).name,
+      most_vice_captained: getPlayer(data.gameweek.most_vice_captained, players).name,
+      total_players: data.total_players,
+    },
+  };
+}
+
+function getDT(data, players) {
+  if (data.error) return { error: 'Dream team not found' };
+
+  const dt = [];
+  for (let i = 0; i < data.team.length; i += 1) {
+    const p = getPlayer(data.team[i].element, players);
+    dt.push({
+      name: p.name,
+      position: p.position,
+      points: data.team[i].points,
+    });
+  }
+
+  return {
+    top_player: {
+      name: getPlayer(data.top_player.id, players).name,
+      points: data.top_player.points,
+    },
+    team: dt,
+  };
+}
+
+async function get(url) {
+  const response = await fetch(url);
 
   if (response.status < 200 || response.status > 299) {
     const status = response.status ? response.status : 500;
@@ -81,22 +152,31 @@ async function fetchStatic() {
   }
 
   const result = await response.json();
+  return result;
+}
 
-  const filteredRes = {
-    events: result.events,
-    total_players: result.total_players,
-    players: result.elements,
-    player_types: result.element_types,
-    timestamp: getDate(),
-  };
+async function fetchStatic() {
+  const result = await get('https://fantasy.premierleague.com/api/bootstrap-static/');
+  if (result.error) return result;
 
-  const jsonResult = JSON.stringify(filteredRes);
+  const gw = getGameWeek(result.events);
+  const staticData = processStatic(
+    { gameweek: gw, total_players: result.total_players },
+    result.elements,
+  );
 
-  fs.writeFile('./static/staticFantasy.json', jsonResult, 'utf8', (err) => {
+  staticData.players = result.elements;
+  staticData.timestamp = getDate();
+
+  const dtResult = await get(`https://fantasy.premierleague.com/api/dream-team/${gw.id}`);
+  staticData.dreamTeam = getDT(dtResult, result.elements);
+
+
+  fs.writeFile('./static/staticFantasy.json', JSON.stringify(staticData), 'utf8', (err) => {
     if (err) console.error('Did not save static');
   });
 
-  return result;
+  return staticData;
 }
 
 function getBootstrap() {
@@ -125,8 +205,9 @@ async function staticFantasy(req, res) {
   }
 
   // get current gameweek stats
+  // fetch dream team -> process and send as part of return
 
-  return res.status(200).json({});
+  return res.status(200).json([staticData.data, staticData.dreamTeam]);
 }
 
 
