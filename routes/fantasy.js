e51be35d0f4861b9bb6ab8d788d7a03b870e3733
arnar44 <arnar44@gmail.com>
2020-04-21 13:15:29 +0200

    Get and save static fantasy data w/ timestamp

--- routes/fantasy.js
+++ routes/fantasy.js
@@ -46,14 +46,63 @@ Static gögn:
 
 const express = require('express');
 const fetch = require('node-fetch');
+const fs = require('fs');
 
 const router = express.Router();
 
-// Calls the scorebat API and returns the current higlights
-async function staticFantasy(req, res) {
+const staticPath = './static/staticFantasy.json';
 
+async function fetchStatic() {
+  const bootstrapUrl = 'https://fantasy.premierleague.com/api/bootstrap-static/';
+  const response = await fetch(bootstrapUrl);
+
+  if (response.status < 200 || response.status > 299) {
+    const status = response.status ? response.status : 500;
+    const statusText = response.statusText ? response.statusText : 'Internal Server Error';
+    return { code: status, error: statusText };
+  }
+
+  const result = await response.json();
+  result.timestamp = Date.now();
+  const jsonResult = JSON.stringify(result);
+
+  fs.writeFile('./static/staticFantasy.json', jsonResult, 'utf8');
+
+  return result;
 }
 
+function getBootstrap() {
+  let fetchNew;
+  let pData;
+  try {
+    const data = fs.readFileSync(staticPath);
+    pData = JSON.parse(data);
+    const timeDiff = parseInt((Date.now() - pData.timestamp) / (60 * 60 * 24 * 1000), 10);
+    fetchNew = timeDiff >= 1;
+  } catch (err) {
+    fetchNew = true;
+  }
+
+  if (fetchNew) {
+    pData = fetchStatic();
+  }
+
+  return pData;
+}
+
+// Returns fantasy data that we want but does not require authentication
+async function staticFantasy(req, res) {
+  const staticData = getBootstrap();
+  if (staticData.error) {
+    return res.status(staticData.status).json({ error: staticData.error });
+  }
+
+  // call some function here that processes static data (or do when we fetch and read)
+  return res.status(200).json(staticData);
+}
+
+
+// Returns fantasy data that requires authentication
 async function myFantasy(req, res) {
 
 }
