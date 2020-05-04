4e055b2e99cbd4544d6a5bccd2c2b2902a204b43
arnar44 <arnar44@gmail.com>
2020-05-04 17:09:52 +0200

    REmove Id from initial users

--- db/sql/insert-users.sql
+++ db/sql/insert-users.sql
@@ -1,13 +1,13 @@
 -- Password: admin123
 INSERT INTO
   users
-    (id, username, email, password, profile)
+    (username, email, password, profile)
   VALUES
-    (1, 'arnarBadoo', 'arnar@badu.org', '$2b$11$7m7YExm1pRpOPLTXJJ6LpuQydXWxKFfwH6eqTPWggZgWZxf75XR0m', 'https://i.ytimg.com/vi/8i0z4oykaUQ/maxresdefault.jpg');
+    ('arnarBadoo', 'arnar@badu.org', '$2b$11$7m7YExm1pRpOPLTXJJ6LpuQydXWxKFfwH6eqTPWggZgWZxf75XR0m', 'https://i.ytimg.com/vi/8i0z4oykaUQ/maxresdefault.jpg');
 
 -- Password: 123123
 INSERT INTO
   users
-    (id, username, email, password, profile)
+    (username, email, password, profile)
   VALUES
-    (2, '_xXxFreysi NettixXx_', 'freyr@leir.is', '$2b$11$1y8dSiIwoSTKjST8M4017.vyG9h0b/guLN/TTYOvOAtUNwNTuEOzC', 'https://miro.medium.com/max/2690/1*O8LQbBqRZjHTOuP2O9Bi1Q.jpeg');
+    ('_xXxFreysi NettixXx_', 'freyr@leir.is', '$2b$11$1y8dSiIwoSTKjST8M4017.vyG9h0b/guLN/TTYOvOAtUNwNTuEOzC', 'https://miro.medium.com/max/2690/1*O8LQbBqRZjHTOuP2O9Bi1Q.jpeg');
