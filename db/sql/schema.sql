c237c5b0f1117bbf488e8dd812482c75d5c233a1
arnar44 <arnar44@gmail.com>
2020-05-04 19:07:36 +0200

    Schema remove unique  teamname

--- db/sql/schema.sql
+++ db/sql/schema.sql
@@ -8,8 +8,9 @@ CREATE TABLE users (
 
 CREATE TABLE teams (
     id SERIAL PRIMARY KEY,
-    team_name VARCHAR(64) NOT NULL UNIQUE,
+    team_name VARCHAR(64) NOT NULL,
     owner_id INTEGER REFERENCES users(id) NOT NULL,
+    owner_username VARCHAR(64) REFERENCES users(username) NOT NULL,
     lineup TEXT NOT NULL, -- Could limit with varchar but no performance difference
     created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp,
     updated TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp
