bbe53e15be76827232237a3d5e72a1de6a852d89
arnar44 <arnar44@gmail.com>
2020-05-04 14:34:04 +0200

    RM id from intial team inserts

--- db/sql/insert-teams.sql
+++ db/sql/insert-teams.sql
@@ -1,13 +1,13 @@
 -- Password: admin123
 INSERT INTO
   teams
-    (id, team_name, owner_id, lineup)
+    (team_name, owner_id, lineup)
   VALUES
-    (1, 'Badoos', 1, '{}');
+    ('Badoos', 1, '{}');
 
 -- lineup: 123123
 INSERT INTO
   teams
-    (id, team_name, owner_id, lineup)
+    (team_name, owner_id, lineup)
   VALUES
-    (2, 'Very cool team', 2, '{}');
+    ('Very cool team', 2, '{}');
