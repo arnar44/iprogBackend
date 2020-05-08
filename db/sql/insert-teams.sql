4455aec79fea9b5cb4ce6d242e9644575ba32ad4
arnar44 <arnar44@gmail.com>
2020-05-09 01:31:36 +0200

    add data

--- db/sql/insert-teams.sql
+++ db/sql/insert-teams.sql
@@ -11,3 +11,152 @@ INSERT INTO
     (team_name, owner_id, owner_username, lineup)
   VALUES
     ('Very cool team', 2, '_xXxFreysi NettixXx_', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('FirstTeam', 2, '_xXxFreysi NettixXx_', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('The Elite', 2, '_xXxFreysi NettixXx_', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('The best', 2, '_xXxFreysi NettixXx_', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('Locos', 2, '_xXxFreysi NettixXx_', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('my team', 3, 'jackson', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('my team 2', 3, 'jackson', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('my team 3', 3, 'jackson', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('Italian stallions', 4, 'Luca', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('Napoli', 4, 'Luca', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('Napoli 2.0', 4, 'Luca', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('The best', 4, 'Luca', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('I LIKE!!!!!', 4, 'Luca', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('Miami heat', 5, 'Aaron', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('Benders', 5, 'Aaron', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('Loopravil', 5, 'Aaron', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('My dream', 5, 'Aaron', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('My dream 2', 5, 'Aaron', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('my shortlist', 5, 'Aaron', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('testTeam', 6, 'testAcc', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('real team', 6, 'testAcc', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('only the best', 6, 'testAcc', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('LA lakers', 6, 'testAcc', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('Danelos', 6, 'testAcc', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+INSERT INTO
+  teams
+    (team_name, owner_id, owner_username, lineup)
+  VALUES
+    ('Eaters', 6, 'testAcc', '{"value":4,"label":"5-2-3","formation":{"gk":["gk"],"def":["lwb","lcb","ccb","rcb","rwb"],"mid":["lcm","rcm"],"att":["lw","st","rw"]},"team":{"gk":{"player_id":18812,"player_name":"Adrián","age":33,"nationality":"Spain","position":"Goalkeeper"},"lwb":{},"lcb":{},"ccb":{},"rcb":{},"rwb":{},"lcm":{},"rcm":{},"lw":{},"st":{},"rw":{}}}');
+
+
+
--- db/sql/insert-users.sql
+++ db/sql/insert-users.sql
@@ -11,3 +11,31 @@ INSERT INTO
     (username, email, password)
   VALUES
     ('_xXxFreysi NettixXx_', 'freyr@leir.is', '$2b$11$1y8dSiIwoSTKjST8M4017.vyG9h0b/guLN/TTYOvOAtUNwNTuEOzC');
+
+-- Password: 123123
+INSERT INTO
+  users
+    (username, email, password)
+  VALUES
+    ('jackson', 'jsk@leir.is', '$2b$11$1y8dSiIwoSTKjST8M4017.vyG9h0b/guLN/TTYOvOAtUNwNTuEOzC');
+
+-- Password: 123123
+INSERT INTO
+  users
+    (username, email, password)
+  VALUES
+    ('Luca', 'toni@italy.is', '$2b$11$1y8dSiIwoSTKjST8M4017.vyG9h0b/guLN/TTYOvOAtUNwNTuEOzC');
+
+-- Password: 123123
+INSERT INTO
+  users
+    (username, email, password)
+  VALUES
+    ('Aaron', 'aron@bolti.is', '$2b$11$1y8dSiIwoSTKjST8M4017.vyG9h0b/guLN/TTYOvOAtUNwNTuEOzC');
+
+-- Password: 123123
+INSERT INTO
+  users
+    (username, email, password)
+  VALUES
+    ('testAcc', 'test@test.is', '$2b$11$1y8dSiIwoSTKjST8M4017.vyG9h0b/guLN/TTYOvOAtUNwNTuEOzC');
