9dcc5c39a1306439f22e1ac47bc3c6bd0ab1317f
arnar44 <arnar44@gmail.com>
2021-04-03 15:07:23 +0200

    Enforce SSL

--- app.js
+++ app.js
@@ -20,6 +20,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  if ((req.get('X-Forwarded-Proto') !== 'https')) {
+    res.redirect('https://' + req.get('Host') + req.url);
+  } else
+    next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/highlights', highlights);
